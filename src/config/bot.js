a0661f7c91b94159eb73a24bed9c5f2d06788d75
cylinsz <141650308+cylinsz@users.noreply.github.com>
2026-06-20 17:04:56 -0400

    Update bot activity message to include author name

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made by Roynel, monitoring 24/7.",
         // Activity type number (0 = Playing).
         type: 0,
       },
